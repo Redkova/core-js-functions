671e53a3e0cd56d21e60c101753b8a88495c714a
Sandra Redkova <sandra_09_94@mail.ru>
2025-04-13 23:41:23 +0200

    feat: implement solution to task getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -53,8 +53,13 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((fn) => {
+    if (typeof fn === 'fn') {
+      return fn.length;
+    }
+    return 0;
+  });
 }
 
 /**
