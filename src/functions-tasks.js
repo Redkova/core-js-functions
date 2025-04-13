ee7b2bf08ec2a80cbac125574466eede62dffd30
Sandra Redkova <sandra_09_94@mail.ru>
2025-04-14 00:17:28 +0200

    feat: implement solution to task memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -121,8 +121,15 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return function memoizedFunction(...args) {
+    const key = args.join(',');
+    if (!(key in cache)) {
+      cache[key] = func(...args);
+    }
+    return cache[key];
+  };
 }
 
 /**
